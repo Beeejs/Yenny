44fee56dd08cc36fa6e053565c09162bac4f4e3a
SitiosWebFM <sitioswebfm01@gmail.com>
2025-09-16 16:56:40 -0300

    Agregamos la clase libro con sus atributos

---
+++ Libro.py
@@ -0,0 +1,8 @@
+class Libro:
+  def __init__(self, id, titulo, autor, precio, categoria, stock):
+    self.id = id
+    self.titulo = titulo
+    self.autor = autor
+    self.precio = precio
+    self.categoria = categoria
+    self.stock = stock
