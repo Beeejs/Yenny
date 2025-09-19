07154a7d74b7b6817702912db0aa73e73009f435
Beeejs <sitioswebfm01@gmail.com>
2025-09-19 20:09:36 -0300

    Agregamos mas clases principales

---
+++ Clases/Usuario.py
@@ -0,0 +1,6 @@
+class Usuario:
+  def __init__(self, id, nombre, rol, password):
+    self.id = id
+    self.nombre = nombre
+    self.rol = rol
+    self.password = password
---
+++ Clases/Venta.py
@@ -0,0 +1,5 @@
+class Venta:
+  def __init__(self, id, fecha, estado):
+    self.id = id
+    self.fecha = fecha
+    self.estado = estado
